b2585194b3c96fa24f419358dd498b1743f9491c
emanuelecomp <cardgameheritage@gmail.com>
2018-02-18 15:10:23 -0300

    finish

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView{
+class NegociacoesView extends View{
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model){
@@ -41,7 +41,5 @@ class NegociacoesView{
         `;
     }
 
-    update(model){
-        this._elemento.innerHTML = this._template(model);
-    }
+
 }
